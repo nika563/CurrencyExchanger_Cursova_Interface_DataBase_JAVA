66bcf90bdd51e622452ce8036f27fb8ed52eaff5
Nikita_Cheater <58823371+nika563@users.noreply.github.com>
2024-06-01 13:14:44 +0300

    add JPanel

--- src/JInterface/adminInterface.java
+++ src/JInterface/adminInterface.java
@@ -3,33 +3,49 @@ package src.JInterface;
 //j_frame
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.TextAttribute;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 //sql
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class adminInterface extends JFrame{
     public adminInterface() {
         super("ADMIN");
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //stop program when exit
+        setExtendedState(JFrame.MAXIMIZED_BOTH); // Разворачиваем окно на весь экран
         super.setBounds(500, 200, 600, 500); //size icon
-
-        /*GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        if (gd.isFullScreenSupported()) {
-            setUndecorated(true);
-            gd.setFullScreenWindow(this);
-        } else {
-            System.err.println("Полноэкранный режим не поддерживается");
-            setSize(100, 100); //Пример размера, если полноэкранный режим не поддерживается
-            setVisible(true);
-        }*/
-
         Container con = super.getContentPane(); //create blocks(button, input ...)
         con.setBackground(Color.decode("#5FB67D"));
 
         JPanel panel = new JPanel(null);
+        JPanel menu = new JPanel();
         panel.setBackground(Color.decode("#5FB67D"));
+        menu.setBackground(Color.decode("#228845"));
+        menu.setPreferredSize(new Dimension(20, 20)); // Задаем предпочтительный размер
+        panel.setPreferredSize(new Dimension(200, 200));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+
 
         JLabel header = new JLabel("Menu admin".toUpperCase());
-        header.setFont(new Font("Arial",  Font.BOLD, 20));
+        Map<TextAttribute, Object> attributes = new HashMap<>();
+        Font currentFont = header.getFont(); // Получаем текущий шрифт метки
+        Font newFont = currentFont.deriveFont(Collections.singletonMap(TextAttribute.WEIGHT, TextAttribute.WEIGHT_SEMIBOLD));
+        attributes.put(TextAttribute.FAMILY, "Arial");
+        header.setFont(newFont.deriveFont(20f)); // Устанавливаем новый шрифт и размер
         header.setForeground(Color.black);
-        header.setBounds(250, 20,100,40);
+        header.setBounds(700, 20,150,40);
+
+
+
+        panel.add(header);
+        con.add(panel);
+        con.add(menu);
+        //send.addActionListener(new eventSerchDataBase()); //add event to button
     }
+    //class eventSerchDataBase implements ActionListener{}
 }
