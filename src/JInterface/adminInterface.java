bfea63bfd0aef15ccbf07483315a533d0029d20b
Nikita_Cheater <58823371+nika563@users.noreply.github.com>
2024-06-01 19:04:10 +0300

    add CardLayout

--- src/JInterface/adminInterface.java
+++ src/JInterface/adminInterface.java
@@ -16,8 +16,65 @@ import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class adminInterface extends JFrame{
+public class adminInterface extends JFrame implements ActionListener{
+    CardLayout card;
+    JButton btn1, btn2, btn3;
+    JPanel card1, card2, card3, cards;
+
     public adminInterface() {
+        // Создаем кнопки для переключения карт
+        btn1 = new JButton("Переключить на Карту 1");
+        btn2 = new JButton("Переключить на Карту 2");
+        btn3 = new JButton("Переключить на Карту 3");
+
+        // Создаем слушателей для кнопок
+        btn1.addActionListener(this);
+        btn2.addActionListener(this);
+        btn3.addActionListener(this);
+
+        // Создаем панели, которые будут "картами"
+        card1 = new JPanel();
+        card1.add(new JLabel("Карта 1"));
+        card1.setBackground(Color.GREEN);
+
+        card2 = new JPanel();
+        card2.add(new JLabel("Карта 2"));
+        card2.setBackground(Color.RED);
+
+        card3 = new JPanel();
+        card3.add(new JLabel("Карта 3"));
+        card3.setBackground(Color.BLUE);
+        // Создаем CardLayout и добавляем к нему карты
+        cards = new JPanel(new CardLayout());
+        cards.add(card1, "Карта 1");
+        cards.add(card2, "Карта 2");
+        cards.add(card3, "Карта 3");
+
+        // Добавляем кнопки и карты в фрейм
+        add(btn1, BorderLayout.NORTH);
+        add(btn2, BorderLayout.SOUTH);
+        add(btn3, BorderLayout.WEST);
+        add(cards, BorderLayout.CENTER);
+
+        // Настройка фрейма
+        setSize(300, 300);
+        //setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public void actionPerformed(ActionEvent e) {
+        card = (CardLayout)(cards.getLayout());
+        if (e.getSource() == btn1) {
+            card.show(cards, "Карта 1");
+        } else if (e.getSource() == btn2) {
+            card.show(cards, "Карта 2");
+        } else if (e.getSource() == btn3) {
+            card.show(cards, "Карта 3");
+        }
+    }
+
+
+
+    /*public adminInterface() {
         super("ADMIN");
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //stop program when exit
         setExtendedState(JFrame.MAXIMIZED_BOTH); // Разворачиваем окно на весь экран
@@ -110,5 +167,5 @@ public class adminInterface extends JFrame{
                 // Обработка клика мыши для deleteData
             }
         });
-    }
+    }*/
 }
